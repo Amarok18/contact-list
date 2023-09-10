05571284b8a7d3de8d717788e8653fb80c8236de
Jose Lopez <jolopez181999@gmail.com>
2023-09-10 10:53:46 -0600

    Agregando arrays de contactos

--- Contactos.js
+++ Contactos.js
@@ -1,4 +1,42 @@
-let Contactos = ["Mario Rivas", "Josue Amador", "Maria Ochoa"];
+let Contactos = [
+    {
+        id:1,
+        nombres: "Marco Antonio",
+        apellidos: "Lopez Arellano",
+        telefono: +50498698754,
+        ubicaciones: [
+            {
+                ciudad: "San Pedro Sula",
+                direccion: "Centro de la Ciudad"
+            }
+        ]
+    },
+    {
+        id:2,
+        nombres: "Francisco David",
+        apellidos: "Chavarria Hernandez",
+        telefono: +50498544754,
+        ubicaciones: [
+            {
+                ciudad: "Lima",
+                direccion: "Frente al Mercado"
+            }
+        ] 
+    },
+    {
+         id:3,
+        nombres: "Maria Luisa",
+        apellidos: "Fernandez Dubon",
+        telefono: +50499698564,
+        ubicaciones: [
+            {
+                ciudad: "Progreso",
+                direccion: "Enfrente de la UTH"
+            }
+        ]
+    }
+    
+]
 //Funcion para agregar contacto
 let contacto = "";
 function AddContact (contacto){
